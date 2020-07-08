d917618f0c229ad99ae023eb6e088ef865133001
Alice <LisaAlisaLis888>
2020-07-08 23:34:34 +0300

    Add UserController

--- ARMDesktopUI/ViewModels/LoginViewModel.cs
+++ ARMDesktopUI/ViewModels/LoginViewModel.cs
@@ -13,6 +13,7 @@ namespace ARMDesktopUI.ViewModels
         private string _userName;
         private string _password;
         private IAPIHelper _apiHelper;
+        private string _errorMessage;
 
         public LoginViewModel(IAPIHelper apiHelper)
         {
@@ -41,6 +42,30 @@ namespace ARMDesktopUI.ViewModels
             }
         }
 
+        public bool IsErrorVisible
+        {
+            get
+            {
+                bool output = false;
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+                return output;
+            }
+        }
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                NotifyOfPropertyChange(() => ErrorMessage);               
+            }
+        }
+
         public bool CanLogIn
         {
             get
@@ -58,11 +83,12 @@ namespace ARMDesktopUI.ViewModels
         {
             try
             {
+                ErrorMessage = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;
             }
         }
     }
