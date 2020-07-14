01d8f112edbd8a7ebb80ea8b0302001f91c56252
Alice <LisaAlisaLis888>
2020-07-14 18:19:04 +0300

    Get Add To Cart Working
    Adds the functionality to add items to the cart,
    get the subtotal, and update quantities.

--- ARMDesktopUI/ViewModels/SalesViewModel.cs
+++ ARMDesktopUI/ViewModels/SalesViewModel.cs
@@ -40,8 +40,20 @@ namespace ARMDesktopUI.ViewModels
             }
         }
 
-        private BindingList<ProductModel> _cart;
-        public BindingList<ProductModel> Cart
+        private ProductModel _selectedProduct;
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set
@@ -51,8 +63,7 @@ namespace ARMDesktopUI.ViewModels
             }
         }
 
-        private int _itemQuantity;
-
+        private int _itemQuantity = 1;
         public int ItemQuantity
         {
             get { return _itemQuantity; }
@@ -60,6 +71,7 @@ namespace ARMDesktopUI.ViewModels
             {
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
 
@@ -67,8 +79,12 @@ namespace ARMDesktopUI.ViewModels
         {
             get
             {
-                // TODO - Replace with calculation
-                return "$0.00";
+                decimal subTotal = 0;
+                foreach (var item in Cart)
+                {
+                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
+                return subTotal.ToString("C");
             }
         }
 
@@ -97,13 +113,37 @@ namespace ARMDesktopUI.ViewModels
                 bool output = false;
                 // Make sure something is selected
                 //Make sure there is an item quantity
+                if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }
                 return output;
             }
         }
 
-        public void AddToCart()
+        public void AddToCard()
         {
-
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                //Hack - There should be a better way of refreshing the cart display
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                CartItemModel item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+                Cart.Add(item);
+            }
+            
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);            
         }
 
         public bool CanRemoveFromCart
@@ -118,7 +158,7 @@ namespace ARMDesktopUI.ViewModels
 
         public void RemoveFromCart()
         {
-
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanCheckOut
