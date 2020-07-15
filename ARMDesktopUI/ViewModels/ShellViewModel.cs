df6195bef5f64e54dc48c40a2c9dda98ca09805b
Alice <LisaAlisaLis888>
2020-07-15 20:00:47 +0300

    Replace Container Reference in ShellVM
    Replace container reference with the IoC reference instead.

--- ARMDesktopUI/ViewModels/ShellViewModel.cs
+++ ARMDesktopUI/ViewModels/ShellViewModel.cs
@@ -9,25 +9,22 @@ using Caliburn.Micro;
 namespace ARMDesktopUI.ViewModels
 {
     public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
-    {        
+    {
         private IEventAggregator _events;
         private SalesViewModel _salesVM;
-        private SimpleContainer _container;
-
-        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM,
-            SimpleContainer container)
-        {           
-            _events = events;            
-            _salesVM = salesVM;            
-            _container = container;
+        
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM)
+        {
+            _events = events;
+            _salesVM = salesVM;
 
             _events.Subscribe(this);
 
-            ActivateItem(_container.GetInstance<LoginViewModel>());
-        }  
+            ActivateItem(IoC.Get<LoginViewModel>());
+        }
         public void Handle(LogOnEvent message)
         {
-            ActivateItem(_salesVM);            
+            ActivateItem(_salesVM);
         }
     }
 }
